e4ba9d4780292a37863b7853337101863fcee2ab
Vadim Samoilov <firstkoks@gmail.com>
2022-04-30 10:49:16 +0300

    add alert

--- toDo/Controller/ViewController.swift
+++ toDo/Controller/ViewController.swift
@@ -7,33 +7,71 @@
 
 import UIKit
 
-class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+class ViewController: UIViewController, UITableViewDataSource {
     
-    let tableView = UITableView()
+    let tableView : UITableView = {
+        let tableView = UITableView()
+        tableView.register(UITableViewCell.self, forCellReuseIdentifier: "cell")
+        
+        return tableView
+    }()
+    
+    var items = [String]()
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        self.items = UserDefaults.standard.stringArray(forKey: "items") ?? []
+        navigationSettings()
         view.addSubview(tableView)
-        tableView.register(UITableViewCell.self, forCellReuseIdentifier: "cell")
-        tableView.delegate = self
         tableView.dataSource = self
     }
-
+    
     override func viewDidLayoutSubviews() {
         super.viewDidLayoutSubviews()
         tableView.frame = view.bounds
     }
-    
+
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 4
+        return items.count
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
-        cell.textLabel?.text = "Cell number: \(indexPath.row + 1)"
-        cell.textLabel?.textAlignment = .center
-
+        cell.textLabel?.text = items[indexPath.row]
+        
         return cell
+    }
+    
+    // MARK: - NavigationBar
+    func navigationSettings() {
+        title = "To Do List"
+        navigationController?.navigationBar.prefersLargeTitles = true
+        navigationItem.rightBarButtonItem = UIBarButtonItem(barButtonSystemItem: .add, target: self, action: #selector(didTapAdd))
+    }
+    
+    @objc func didTapAdd() {
+        let alert = UIAlertController(title: "New Item", message: "enter new to do list item", preferredStyle: .alert)
+        
+        alert.addTextField { field in
+            field.placeholder = "enter new item..."
+        }
+        alert.addAction(UIAlertAction(title: "Cancel", style: .cancel, handler: nil))
+        alert.addAction(UIAlertAction(title: "Done", style: .default, handler: { [weak self] (_) in
+            if let field = alert.textFields?.first {
+                if let text = field.text, !text.isEmpty {
+                    // enter new to do list item
+                    DispatchQueue.main.async {
+                        var currentItems = UserDefaults.standard.stringArray(forKey: "items") ?? []
+                        currentItems.append(text)
+                        UserDefaults.standard.setValue(currentItems, forKey: "items")
+                        self?.items.append(text)
+                        self?.tableView.reloadData()
+                    }
+                    
+                }
+            }
+        }))
+        present(alert, animated: true, completion: nil)
     }
 }
 
